c493acc8f7395da923ed47ea8d7ec8d5e40ba323
Vo1dling <attlanticazz@hotmail.com>
2022-06-09 11:58:01 +0300

    Fixed Wrong Link

--- src/components/project-page/project-page.component.ts
+++ src/components/project-page/project-page.component.ts
@@ -15,7 +15,7 @@ export class ProjectPageComponent implements OnInit {
       description:
         'This is a mockup computer store,It was built using only HTML5 and CSS3 and is one of my first websites (2nd week of the bootcamp)',
       img: '../../assets/imgs/QWERTY-Hero.png',
-      demoLink: 'https://apple-weekend.netlify.app/',
+      demoLink: 'https://QWERTY-IT.netlify.app/',
       codeLink: 'https://github.com/Blackzero172/QWERTY',
       icons: ['html', 'css'],
     },
