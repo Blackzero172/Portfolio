527500902cd4edd882bbb80bef296a5aed41de0a
Blackzero172 <Supermeega@outlook.com>
2023-01-02 13:43:18 +0200

    Changed Websites Links

--- src/components/project-page/project-page.component.ts
+++ src/components/project-page/project-page.component.ts
@@ -62,7 +62,7 @@ export class ProjectPageComponent implements OnInit {
       description:
         "Last but certainly not Least, my Final Project in the bootcamp ,my main reasons for this one is due to the fact that most workplaces use either screenshots of Excel Sheets and the problem with that method is that you can't easily modify and change the schedule and everytime you do you need to notify your workers and this website is a much better alternative for this kind of work ",
       img: '../../assets/imgs/End-Hero.png',
-      demoLink: 'https://ali-shift-scheduler.herokuapp.com/',
+      demoLink: 'https://ali-shift-scheduler.azurewebsites.net/',
       codeLink: 'https://github.com/Blackzero172/End_Project',
       icons: ['react', 'css', 'node', 'mongo'],
     },
